f02445e0decb7b22f687d207d4e359a014808e37
Luke Harris <lukeharris01170@gmail.com>
2018-11-27 08:59:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var allowCrossDomain = function(req, res, next) {
 app.use(bodyParser.json());
 app.use(allowCrossDomain);
 
-mongoose.connect(proccess.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
